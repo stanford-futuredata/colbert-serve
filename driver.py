1d77addd8e62991db5724eec3866ae8a70afe9a5
thejas <thejasvenkatesh97@gmail.com>
2024-05-29 19:17:43 -0700

    Add colbert serving code

---
+++ driver.py
@@ -0,0 +1,136 @@
+import sys
+
+import grpc
+import asyncio
+import psutil
+import server_pb2
+import server_pb2_grpc
+import signal
+from subprocess import Popen
+import argparse
+import time
+import os
+from multiprocessing.connection import Client
+from colbert.data import Queries
+
+def save_rankings(rankings, filename):
+    output = []
+    for q in rankings:
+        for result in q.topk:
+            output.append("\t".join([str(x) for x in [q.qid, result.pid, result.rank, result.score]]))
+
+    f = open(filename, "w")
+    f.write("\n".join(output))
+    f.close()
+
+
+async def run_request(stub, request, experiment):
+    t = time.time()
+    if experiment == "search":
+        out = await stub.Search(request)
+    elif experiment == "pisa":
+        out = await stub.Pisa(request)
+    else:
+        out = await stub.Serve(request)
+
+    return out, time.time() - t
+
+
+async def run(args):
+    print("Main process running on CPU", psutil.Process().cpu_num())
+    nodes = args.num_servers
+    queries = Queries(path=f"/home/ubuntu/data/{args.index}/questions.tsv")
+    qvals = list(queries.items())
+    tasks = []
+    channels = []
+    stubs = []
+
+    for i in range(nodes):
+        channels.append(grpc.aio.insecure_channel('localhost:5005' + str(i)))
+        stubs.append(server_pb2_grpc.ServerStub(channels[-1]))
+
+    inter_request_time = [float(x) for x in open(args.timings).read().split("\n") if x != ""]
+    length = len(inter_request_time)
+
+    # Warmup
+    for i in range(len(qvals)-100, len(qvals)):
+        request = server_pb2.Query(query=qvals[i][1], qid=qvals[i][0], k=100)
+        tasks.append(asyncio.ensure_future(run_request(stubs[i % nodes], request, args.experiment)))
+        await asyncio.sleep(0)
+
+    await asyncio.gather(*tasks)
+
+    print("Warmup complete!")
+
+    tasks = []
+    t = time.time()
+
+    for i in range(2000):
+        print(i, channels[i % nodes])
+        request = server_pb2.Query(query=qvals[i][1], qid=qvals[i][0], k=100)
+        tasks.append(asyncio.ensure_future(run_request(stubs[i % nodes], request,  args.experiment)))
+        await asyncio.sleep(inter_request_time[i % length])
+
+    await asyncio.sleep(0)
+    ret = list(zip(*await asyncio.gather(*tasks)))
+
+    save_rankings(ret[0], args.ranking_file)
+
+    total_time = str(time.time()-t)
+
+    open(args.output, "w").write("\n".join([str(x) for x in ret[1]]) + f"\nTotal time: {total_time}")
+    print(f"Total time for {len(qvals)-100} requests:",  total_time)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Evaluator for ColBERT')
+    parser.add_argument('-w', '--num_workers', type=int, required=True,
+                       help='Number of worker threads per server')
+    parser.add_argument('-n', '--num_servers', type=int, required=True,
+                        help='Number of servers')
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help='Output file to save results')
+    parser.add_argument('-r', '--ranking_file', type=str, required=True,
+                        help='Output file to save rankings')
+    parser.add_argument('-t', '--timings', type=str, required=True,
+                        help='Input file for inter request wait times')
+    parser.add_argument('-s', '--skip_encoding', action='store_true',
+                        help='Use precomputed encoding')
+    parser.add_argument('-e', '--experiment', type=str, default="search", choices=["search", "pisa", "serve"],
+                        help='search or pisa or serve (pisa + rerank)')
+    parser.add_argument('-i', '--index', type=str, choices=["wiki", "lifestyle"],
+                        required=True, help='Index to run')
+
+    processes = []
+    args = parser.parse_args()
+
+    if args.num_servers > psutil.cpu_count():
+        print("Not enough CPUs, exiting!")
+        sys.exit(-1)
+
+    for node in range(args.num_servers):
+        print("Starting process", node)
+        arg_str = f"-w {args.num_workers} -i {args.index}" 
+        if args.skip_encoding: arg_str += " -s"
+        processes.append(Popen(["python", "eval_server.py"] + f"{arg_str}".split(" ")))
+
+        times = 10
+        for i in range(times):
+            try:
+                connection = Client(('localhost', 50040), authkey=b'password')
+                assert connection.recv() == "Done"
+                connection.close()
+                break
+            except ConnectionRefusedError:
+                if i == times - 1:
+                    print("Failed to receive connection for child server. Terminating!")
+                    for p in processes:
+                        os.killpg(os.getpgid(p), signal.SIGTERM)
+                    sys.exit(-1)
+                time.sleep(5)
+
+    asyncio.run(run(args))
+
+    for p in processes:
+        print("Killing processing after completion")
+        p.kill()
---
+++ server.py
@@ -0,0 +1,183 @@
+import argparse
+import sys
+import grpc
+from concurrent import futures
+import torch
+import server_pb2
+import server_pb2_grpc
+import psutil
+from multiprocessing.connection import Listener
+import gc
+from colbert.data import Queries
+import os.path
+import json
+
+import time
+import csv
+
+from colbert import Searcher
+from collections import defaultdict
+import requests
+from colbert.infra.run import Run
+from colbert.infra.config import ColBERTConfig
+
+sys.path.append(os.environ["SPLADE_PATH"])
+
+import splade_pb2
+import splade_pb2_grpc
+
+class ColBERTServer(server_pb2_grpc.ServerServicer):
+    def __init__(self, num_workers, index, skip_encoding):
+        self.tag = 0
+        self.threads = num_workers
+        self.suffix = ""
+        self.index_name = "wiki.2018.latest" if index == "wiki" else "lifestyle.dev.nbits=2.latest"
+        self.multiplier = 250 if index == "wiki" else 500
+        self.index_name += self.suffix
+        prefix = "/home/ubuntu/data"
+        gold_rankings_files = f"{prefix}/{index}/rankings.tsv"
+        self.gold_ranks = defaultdict(list)
+        self.skip_encoding = skip_encoding
+
+        channel = grpc.insecure_channel('localhost:50060')
+        self.splade_stub = splade_pb2_grpc.SpladeStub(channel)
+
+        with open(gold_rankings_files, newline='', encoding='utf-8') as tsvfile:
+            tsv_reader = csv.reader(tsvfile, delimiter='\t')
+            for line in tsv_reader:
+                self.gold_ranks[int(line[0])].append(int(line[1]))
+        
+        config = Run().config
+        config = ColBERTConfig.from_existing(None, config)
+        config.load_index_with_mmap = self.suffix != ""
+        print(config)
+        self.searcher = Searcher(index=f"{prefix}/indexes/{self.index_name}/", config=config)
+        self.ranker = self.searcher.ranker
+        
+        if os.path.isfile(f"{prefix}/{index}/encodings.pt"):
+            self.enc = torch.load(f"{prefix}/{index}/encodings.pt")
+        elif skip_encoding:
+            queries = Queries(path=f"{prefix}/{index}/questions.tsv")
+            qvals = list(queries.items())
+            self.enc = {}
+            for q in qvals:
+                self.enc[q[0]] = self.searcher.encode([q[1]])
+            torch.save(self.enc, f"{prefix}/{index}/encodings.pt")
+
+    def convert_dict_to_protobuf(self, input_dict):
+        query_result = server_pb2.QueryResult()
+
+        query_result.qid = input_dict["qid"]
+
+        for topk_dict in input_dict["topk"]:
+            topk_result = query_result.topk.add()
+            topk_result.pid = topk_dict["pid"]
+            topk_result.rank = topk_dict["rank"]
+            topk_result.score = topk_dict["score"]
+
+        return query_result
+    
+    def api_serve_query(self, query, qid, k=100):
+        t2 = time.time()
+        url = 'http://localhost:8080'
+        splade_q = self.splade_stub.GenerateQuery(splade_pb2.QueryStr(query=query, multiplier=self.multiplier))
+        data = {"query": splade_q.query, "k": 200}
+        headers = {'Content-Type': 'application/json'}
+        
+        response = requests.post(url, data=json.dumps(data), headers=headers).text
+        response = json.loads(response).get('results', {})
+        gr = torch.tensor([int(x) for x in response.keys()], dtype=torch.int)
+        Q = self.searcher.encode([query]) if not self.skip_encoding else self.enc[qid]
+        scores_, pids_ = self.ranker.score_raw_pids(self.searcher.config, Q, gr)
+        print("Searching time of {} on node {}: {}".format(qid, self.tag, time.time() - t2))
+
+        scores_sorter = scores_.sort(descending=True)
+        pids_, scores_ = pids_[scores_sorter.indices].tolist(), scores_sorter.values.tolist()
+        top_k = []
+        for pid, rank, score in zip(pids_, range(len(pids_)), scores_):
+            top_k.append({'pid': pid, 'rank': rank + 1, 'score': score})
+
+        return self.convert_dict_to_protobuf({"qid": qid, "topk": top_k[:k]})
+
+    def api_search_query(self, query, qid, k=100):
+        t2 = time.time()
+        if not self.skip_encoding:
+            pids, ranks, scores = self.searcher.search(query, k)
+        else:
+            pids, ranks, scores = self.searcher.dense_search(self.enc[qid], k)
+        print("Searching time of {}: {}".format(qid, time.time() - t2))
+
+        top_k = []
+        for pid, rank, score in zip(pids, ranks, scores):
+            top_k.append({'pid': pid, 'rank': rank, 'score': score})
+        top_k = list(sorted(top_k, key=lambda p: (-1 * p['score'], p['pid'])))
+
+        return self.convert_dict_to_protobuf({"qid": qid, "topk": top_k})
+
+    def api_pisa_query(self, query, qid, k=100):
+        t2 = time.time()
+        splade_q = self.splade_stub.GenerateQuery(splade_pb2.QueryStr(query=query, multiplier=self.multiplier))
+        # print("Splade time", time.time()-t2)
+        url = 'http://localhost:8080'
+        data = {"query": splade_q.query, "k": 200}
+        # data = {"query": query, "k": 200}
+        headers = {'Content-Type': 'application/json'}
+
+        tpost = time.time()
+        response = requests.post(url, data=json.dumps(data), headers=headers).text
+        response = json.loads(response).get('results', {})
+        # print("Searching time of {} on node {}: {}".format(qid, self.tag, time.time() - t2))
+
+        pids_ = []
+        scores_ = []
+
+        for kk, v in response.items():
+            pids_.append(int(kk))
+            scores_.append(float(v))
+
+        print("Searching time of {} on node {}: {}".format(qid, self.tag, time.time() - t2))
+
+        top_k = []
+        for pid, rank, score in zip(pids_, range(len(pids_)), scores_):
+            top_k.append({'pid': pid, 'rank': rank + 1, 'score': score})
+
+        return self.convert_dict_to_protobuf({"qid": qid, "topk": top_k[:k]})
+
+    def Search(self, request, context):
+        torch.set_num_threads(self.threads)
+        return self.api_search_query(request.query, request.qid, request.k)
+
+    def Serve(self, request, context):
+        torch.set_num_threads(self.threads)
+        return self.api_serve_query(request.query, request.qid, request.k)
+
+    def Pisa(self, request, context):
+        torch.set_num_threads(self.threads)
+        return self.api_pisa_query(request.query, request.qid, request.k)
+
+
+def serve_ColBERT_server(args):
+    connection = Listener(('localhost', 50040), authkey=b'password').accept()
+    server = grpc.server(futures.ThreadPoolExecutor())
+    server_pb2_grpc.add_ServerServicer_to_server(ColBERTServer(args.num_workers, args.index, args.skip_encoding), server)
+    listen_addr = '[::]:50050'
+    server.add_insecure_port(listen_addr)
+    print(f"Starting ColBERT server on {listen_addr}")
+    connection.send("Done")
+    connection.close()
+    server.start()
+    server.wait_for_termination()
+    print("Terminated")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Server for ColBERT')
+    parser.add_argument('-w', '--num_workers', type=int, required=True,
+                       help='Number of worker threads per server')
+    parser.add_argument('-s', '--skip_encoding', action='store_true',
+                        help='Use precomputed encoding')
+    parser.add_argument('-i', '--index', type=str, choices=["wiki", "lifestyle"],
+                        required=True, help='Index to run')
+
+    args = parser.parse_args()
+    serve_ColBERT_server(args)
